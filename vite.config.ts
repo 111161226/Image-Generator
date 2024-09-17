9ee741764d32d79b41b33800905f282d1ae53958
111161226 <fanrin9@gmail.com>
2024-09-18 03:42:36 +0900

    modify config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Image-Generator"  ,          // この行を追加
+  base: process.env.GITHUB_PAGES ? "Image-Generator" : './' ,          // この行を追加
   plugins: [react()],
 })
