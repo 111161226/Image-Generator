fe26a3f6ce613fa55dadc26e2a6762ff157f4688
111161226 <fanrin9@gmail.com>
2026-05-04 21:20:50 +0900

    fix base url in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Image-Generator/",          // 前後のスラッシュを確認してください
+export default defineConfig({  base: "/", 
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        // ライブラリを別ファイルに分割して500kB警告を回避する
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return 'vendor';
@@ -16,7 +13,6 @@ export default defineConfig({
         }
       }
     },
-    // チャンクサイズの警告しきい値を1000kBに引き上げる
     chunkSizeWarningLimit: 1000,
   }
 })
